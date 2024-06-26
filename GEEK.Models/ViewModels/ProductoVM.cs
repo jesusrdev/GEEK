c977e16dcff2955f8d10e1c4aaa9f35ed58d6382
jesus <rojasalarcone@gmail.com>
2024-06-26 07:27:49 -0500

    Creando el controlador y la vista para productos asi como su viewmodel

---
+++ GEEK.Models/ViewModels/ProductoVM.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GEEK.Models.ViewModels
+{
+    internal class ProductoVM
+    {
+        public Producto Producto { get; set; }
+
+        public IEnumerable<SelectListItem>? ListaCategorias { get; set; }
+        public IEnumerable<SelectListItem>? ListaMarcas { get; set; }
+
+    }
+}
---
+++ GEEK/Areas/Admin/Controllers/ProductosController.cs
@@ -0,0 +1,40 @@
+﻿using BlogCore.AccesoDatos.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GEEK.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductosController : Controller
+    {
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public ProductosController(IContenedorTrabajo contenedorTrabajo, IWebHostEnvironment hostingEnvironment)
+        {
+            _contenedorTrabajo = contenedorTrabajo;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        // Para este controller se usara productoVM
+
+        #region Llamadas a la API - metodos que usa ajax
+
+        public IActionResult GetAll()
+        {
+            // Traer la lista de productos con el GetAll
+            return Json(new {});
+        }
+
+        #endregion
+    }
+}
