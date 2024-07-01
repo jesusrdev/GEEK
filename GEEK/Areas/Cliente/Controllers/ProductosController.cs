fbb474d378862ae4ce82e3313d55d13e684055f9
jesus <rojasalarcone@gmail.com>
2024-07-01 05:59:29 -0500

    Maquetacion de la pagina de productos

---
+++ GEEK/Areas/Cliente/Controllers/ProductosController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GEEK.Areas.Cliente.Controllers
+{
+    [Area("Cliente")]
+    public class ProductosController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            ViewBag.IsProductosActive = "active";
+
+            return View();
+        }
+    }
+}
