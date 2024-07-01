7f9050fec2d7e495418ad3cbd6ef8fd4e20429f4
jesus <rojasalarcone@gmail.com>
2024-07-01 07:10:40 -0500

    Creando dinamicamente el slider de marcas

--- GEEK/Areas/Cliente/Controllers/HomeController.cs
+++ GEEK/Areas/Cliente/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using GEEK.Models;
+﻿using GEEK.AccesoDatos.Data.Repository.IRepository;
+using GEEK.Models;
+using GEEK.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,17 +9,26 @@ namespace GEEK.Areas.Cliente.Controllers
     [Area("Cliente")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
+        private readonly IContenedorTrabajo _contenedorTrabajo;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(IContenedorTrabajo contenedorTrabajo)
         {
-            _logger = logger;
+            _contenedorTrabajo = contenedorTrabajo;
         }
 
         public IActionResult Index()
         {
+            var HomeVM = new HomeVM()
+            {
+                Productos = _contenedorTrabajo.Producto.GetAll(),
+                Categorias = _contenedorTrabajo.Categoria.GetAll(),
+                Marcas = _contenedorTrabajo.Marca.GetAll()
+            };
+
             ViewBag.IsHomeActive = "active";
-            return View();
+            return View(HomeVM);
         }
 
         
