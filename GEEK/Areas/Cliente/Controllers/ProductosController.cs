b4082227f3f8ef4e90563c5fdfa946f12e376818
jesus <rojasalarcone@gmail.com>
2024-07-01 08:01:24 -0500

    Creando la lista de marcas y categorias de manera dinamica en las paginas de Home y Productos

--- GEEK/Areas/Cliente/Controllers/ProductosController.cs
+++ GEEK/Areas/Cliente/Controllers/ProductosController.cs
@@ -1,16 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GEEK.AccesoDatos.Data.Repository.IRepository;
+using GEEK.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GEEK.Areas.Cliente.Controllers
 {
     [Area("Cliente")]
     public class ProductosController : Controller
     {
-        public IActionResult Index()
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+
+        public ProductosController(IContenedorTrabajo contenedorTrabajo)
         {
+            _contenedorTrabajo = contenedorTrabajo;
+        }
+
+        public IActionResult Index(string idMarca)
+        {
+            var HomeVM = new HomeVM()
+            {
+                Productos = _contenedorTrabajo.Producto.GetAll(),
+                Categorias = _contenedorTrabajo.Categoria.GetAll(),
+                Marcas = _contenedorTrabajo.Marca.GetAll()
+            };
+
+            ViewData["idMarca"] = idMarca;
 
             ViewBag.IsProductosActive = "active";
 
-            return View();
+            return View(HomeVM);
         }
     }
 }
